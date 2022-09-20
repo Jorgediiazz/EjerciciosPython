09ffbac0c271ebe58da95a789a4a230243813ad3
Jorgediiazz <113972610+Jorgediiazz@users.noreply.github.com>
2022-09-20 12:17:45 +0200

    Add files via upload

---
+++ ej2.py
@@ -0,0 +1,11 @@
+#Realizar un programa que sea capaz de convertir los grados centígrados en grados Farenheit y viceversa.
+celsius=0
+farenheit=0
+#Pedir grados celsius
+celsius=float(input("Introduce los grados celsius:"))
+
+#Realizar la operación
+farenheit=1.8*celsius+32
+
+#Dar el resultado
+print("El resultado es",farenheit, " grados Farenheit")
